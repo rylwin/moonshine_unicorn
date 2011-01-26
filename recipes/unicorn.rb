316308053cdd24c0f854b46f8d91243583976609
Josh Goebel <dreamer3@gmail.com>
2011-01-26 14:30:56 -0500

    rotate the unicorn process after a deploy

---
+++ recipes/unicorn.rb
@@ -0,0 +1,20 @@
+namespace :unicorn do
+  desc <<-DESC
+  Restart unicorn
+  DESC
+  task :upgrade do
+    sudo "/etc/init.d/unicorn upgrade"
+  end
+
+  desc "stop unicorn"
+  task :stop do
+    sudo "/etc/init.d/unicorn stop"
+  end
+
+  desc "start unicorn"
+  task :start do
+    sudo "/etc/init.d/unicorn start"
+  end
+
+end
+after 'deploy', 'unicorn:upgrade'
